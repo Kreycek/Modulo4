507378c2603d8da9bb0b240a4bd88f24a7223362
kreycek <kreycek@gmail.com>
2021-03-08 21:32:39 -0300

    adsa

---
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,17 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Rabbit.Infra.Bus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Infra.IoC
+{
+   public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus,RabbitMQBus>
+        }
+    }
+}
